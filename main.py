fedd174ad277e0b6a7ab671cc33029ecfca5f9f7
Ricardo Quintero <rquinterog93@gmail.com>
2025-01-26 21:32:17 +0100

    concats both ragDB data and nerDB data

--- main.py
+++ main.py
@@ -12,27 +12,47 @@ def main():
 
     # Load data
     st.header("Loading data...")
-    df_gs_cards = get_globalstates_cards()
-    df_gs_retrievalCount = get_globalstates_retrievalCount()
+    df_ragdb_gs_cards = get_globalstates_cards()
+    df_ragdb_gs_retrievalCount = get_globalstates_retrievalCount()
+    df_nerdb_gs_cards = get_globalstates_cards(db="nerDB")
+    df_nerdb_gs_retrievalCount = get_globalstates_retrievalCount(db="nerDB")
 
     # Merge DataFrames
     st.header("Merging data...")
-    merged_df = pd.merge(df_gs_cards, df_gs_retrievalCount, on="id", how="left")
-    merged_df = merged_df.dropna(subset=["metadata.timestamp"]).reset_index()
+    ragdb_merged_df = pd.merge(
+        df_ragdb_gs_cards, df_ragdb_gs_retrievalCount, on="id", how="left"
+    )
+    ragdb_merged_df = ragdb_merged_df.dropna(
+        subset=["metadata.timestamp"]
+    ).reset_index()
 
     # Ensure metadata.timestamp is converted to Python datetime
-    merged_df["metadata.timestamp"] = pd.to_datetime(
-        merged_df["metadata.timestamp"].astype(int), unit="ms"
+    ragdb_merged_df["metadata.timestamp"] = pd.to_datetime(
+        ragdb_merged_df["metadata.timestamp"].astype(int), unit="ms"
     ).dt.to_pydatetime()
 
+    nerdb_merged_df = pd.merge(
+        df_nerdb_gs_cards, df_nerdb_gs_retrievalCount, on="id", how="left"
+    )
+    nerdb_merged_df = nerdb_merged_df.dropna(
+        subset=["metadata.timestamp"]
+    ).reset_index()
+
+    # Ensure metadata.timestamp is converted to Python datetime
+    nerdb_merged_df["metadata.timestamp"] = pd.to_datetime(
+        nerdb_merged_df["metadata.timestamp"].astype(int), unit="ms"
+    ).dt.to_pydatetime()
+
+    mongo_merge_df = pd.concat([ragdb_merged_df, nerdb_merged_df])
+
     # Display DataFrame in Streamlit (optional)
-    st.subheader("Merged DataFrame Preview")
-    st.dataframe(merged_df)
+    st.subheader("Data Preview")
+    st.dataframe(mongo_merge_df)
 
     # Add a timestamp filter slider
     st.header("Filter by Timestamp")
-    min_time = merged_df["metadata.timestamp"].min()
-    max_time = merged_df["metadata.timestamp"].max()
+    min_time = mongo_merge_df["metadata.timestamp"].min()
+    max_time = mongo_merge_df["metadata.timestamp"].max()
 
     # Convert min_time and max_time to datetime.datetime explicitly
     min_time = datetime.datetime.fromisoformat(min_time.isoformat())
@@ -52,9 +72,9 @@ def main():
 
     # Generate the network graph based on the selected time range
     st.header("Generating the network graph...")
-    filtered_df = merged_df[
-        (merged_df["metadata.timestamp"] >= start_time)
-        & (merged_df["metadata.timestamp"] <= end_time)
+    filtered_df = mongo_merge_df[
+        (mongo_merge_df["metadata.timestamp"] >= start_time)
+        & (mongo_merge_df["metadata.timestamp"] <= end_time)
     ]
     network_graph = make_network_graph(filtered_df)
 
